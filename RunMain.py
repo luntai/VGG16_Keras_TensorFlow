911979b4597a687da84878ff1ad51367288092f1
JinxuLi <396974197@qq.com>
2017-04-30 15:52:05 +0800

    add top 3 results

--- RunMain.py
+++ RunMain.py
@@ -2,12 +2,16 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-import cv2, numpy as np
+
+import cv2
+import numpy as np
 
 from keras import backend as K
 K.set_image_dim_ordering('tf')
-"image_dim_ordering in 'th' mode the channels dimension (the depth) is at index 1 (e.g. 3, 256, 256). " \
-"In 'tf' mode is it at index 3 (e.g. 256, 256, 3)."
+'''
+image_dim_ordering in 'th' mode the channels dimension (the depth) is at index 1 (e.g. 3, 256, 256). " \
+"In 'tf' mode is it at index 3 (e.g. 256, 256, 3).
+'''
 
 
 def VGG_16(weights_path=None):
@@ -32,13 +36,13 @@ def VGG_16(weights_path=None):
     model.add(Convolution2D(256, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(512, 3, 3, activation='relu'))
@@ -61,7 +65,7 @@ def VGG_16(weights_path=None):
     return model
 
 if __name__ == "__main__":
-    im = cv2.resize(cv2.imread('./data//image//cat1.jpg'),
+    im = cv2.resize(cv2.imread('./data//image//zebra.jpg'),
                     (224, 224)).astype(np.float32)
     # normalization
     # The mean pixel values are taken from the VGG authors, which are the values computed from the training dataset.
@@ -76,7 +80,13 @@ if __name__ == "__main__":
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     out = model.predict(im)
-    print(np.argmax(out))
+    # print(out.size)
+    # print(np.argmax(out))
+    ordered_index = np.argsort(-out)  # descend sort return index array
+    print(ordered_index)
     import CallResult
-    print(CallResult.lines[np.argmax(out)])
+    for i in range(0, 3):
+        print(CallResult.lines[int(ordered_index[0][i])])
+
+    # print(CallResult.lines[np.argmax(out)])
 
