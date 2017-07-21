f018ad9d472774121ea91ae38e04f377a3da4538
jusjusjus <jschwabedal@gmail.com>
2017-07-20 23:25:34 -0400

    Added a program around vgg16 classification.
    Using matplotlib.pyplot, the image is shown including the top-3 classes.
    Option --input can be used to classify a specific image;  note that the
    image is still resized to 224x224.

--- main.py
+++ main.py
@@ -64,29 +64,59 @@ def VGG_16(weights_path=None):
 
     return model
 
+
+def get_classes():
+    with open('./data/model/synset_words.txt', 'r') as f: 
+        lines = f.readlines()
+    return np.array(lines)
+
+def parse_args():
+
+    import os
+    import argparse
+
+    description = 'Classify an image with vgg16.'
+    prog = os.path.basename(__file__)
+    parser = argparse.ArgumentParser(prog=prog, description=description)
+    parser.add_argument('-i', '--input', type=str, default='data/image/zebra.jpg', help='Picture to classify.')
+    args = parser.parse_args()
+    assert os.path.exists(args.input), "File '%s' doesn't exist." % (args.input)
+    return args
+
+
 if __name__ == "__main__":
-    im = cv2.resize(cv2.imread('./data//image//zebra.jpg'),
-                    (224, 224)).astype(np.float32)
+
+    import matplotlib.pyplot as plt
+
+    args = parse_args()
+
+    l, w = 224, 224
+
+    im_original = cv2.imread(args.input)
+    im = cv2.resize(im_original, (224, 224))
+    im = im.astype(np.float32)
     # normalization
     # The mean pixel values are taken from the VGG authors, which are the values computed from the training dataset.
     im[:, :, 0] -= 103.939
     im[:, :, 1] -= 116.779
     im[:, :, 2] -= 123.68
-    im = im.transpose((1, 0, 2))
     im = np.expand_dims(im, axis=0)
 
     # Test pretrained model
-    model = VGG_16('./data//model//vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+    model = VGG_16('./data/model/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     out = model.predict(im)
-    # print(out.size)
-    # print(np.argmax(out))
-    ordered_index = np.argsort(-out)  # descend sort return index array
-    print(ordered_index)
-    import CallResult
-    for i in range(0, 3):
-        print(CallResult.lines[int(ordered_index[0][i])])
 
-    # print(CallResult.lines[np.argmax(out)])
+    # descend sort prediction, and find top-3 classes
+    ordered_index = np.argsort(-out)[0]
+    all_classes = get_classes()
+    classes = '\n'.join(all_classes[ordered_index[0:3]])
 
+    plt.figure(figsize=(10, 6))
+    plt.subplot(121, xticks=[], yticks=[], frameon=False)
+    plt.imshow(im_original)
+    plt.subplot(122, xticks=[], yticks=[], frameon=False)
+    plt.text(0.5, 0.5, classes, ha='center', va='center', fontsize=14)
+    plt.tight_layout()
+    plt.show()
